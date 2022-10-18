cc8395138accc742a1a65e2568f7f7877e602d71
Nazar <nazar.khatsko@gmail.com>
2022-10-18 12:05:28 +0300

    Last commit

--- src/api/orders.ts
+++ src/api/orders.ts
@@ -3,12 +3,12 @@ import { API_URL } from "../config"
 
 class OrdersApi {
   async create(blockchain: string, address: string, keywords: string, email: string) {
-    await axios.post(`${API_URL}/api/v1/orders/create`, {
-      blockchain: blockchain,
-      address: address,
-      keywords: keywords,
-      email: email
-    })
+    // await axios.post(`${API_URL}/api/v1/orders/create`, {
+    //   blockchain: blockchain,
+    //   address: address,
+    //   keywords: keywords,
+    //   email: email
+    // })
   }
 
   async find(id: string) {}
